9f1c668029daeb29273850f6943c9a92041363a0
isha gohel <ishagohel181@gmail.com>
2026-03-21 12:18:11 +0530

    solved aync hadler issue in the user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -61,16 +61,11 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
-  try {
-    const salt = await bcryptjs.genSalt(parseInt(process.env.BCRYPT_ROUNDS) || 10);
-    this.password = await bcryptjs.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcryptjs.genSalt(parseInt(process.env.BCRYPT_ROUNDS) || 10);
+  this.password = await bcryptjs.hash(this.password, salt);
 });
 
 // Method to compare passwords
